8f70fb2f3907c059fe316775ed003b64f2d7efbf
Dudel <dudel@demogr.mpg.de>
2025-07-28 14:13:34 +0200

    Added bootstrap and confidence intervals to risk ratios for job quality

--- Code/4-jobquality.R
+++ Code/4-jobquality.R
@@ -79,6 +79,86 @@
   quality <- cbind(quality,riskratio)
   
   
+### Bootstrap ##################################################################
+  
+  # Number of replications
+  replications <- 500
+  
+  # For results
+  bootresults <- list()
+  
+  # List of individuals and rows
+  ids <- unique(regdat$id)
+  nids <- length(ids)
+  regdat$nr <- 1:dim(regdat)[1]
+  rows <- by(regdat$nr,regdat$id,function(x) return(x))
+  
+  # Loop for resampling
+  for(rep in 1:replications) {
+    
+    # Output
+    cat(".")
+    if(rep%%50==0) cat("\n")
+    
+    # Get sample
+    whichids <- sample(1:nids,size=nids,replace=T)
+    whichrows <- unlist(as.list(rows)[whichids])
+    
+    # Get data
+    tmpdat <- regdat[whichrows,]
+    
+    # Regression models
+    physical_logit <- glm(physical ~ gender*race + gender*education + race*education + gender*age + gender*age2 + gender*stateboth,
+                          family=binomial,
+                          data=tmpdat)
+    
+    stress_logit <- glm(stress ~ gender*race + gender*education + race*education + gender*age + gender*age2 + gender*stateboth,
+                        family=binomial,
+                        data=tmpdat)
+    
+    poverty_logit <- glm(poverty ~ gender*race + gender*education + race*education + gender*age + gender*age2 + gender*stateboth,
+                         family=binomial,
+                         data=tmpdat)
+    
+    # Predict outcomes
+    regpre$physical <- predict(physical_logit,regpre,type="response")
+    regpre$stress <- predict(stress_logit,regpre,type="response")
+    regpre$poverty <- predict(poverty_logit,regpre,type="response")
+    
+    # Format results
+    tmp1 <- regpre |> filter(stateboth=="working/unhealthy")
+    tmp2 <- regpre |> filter(stateboth=="working/healthy")
+    tmpratio <- tmp1[,7:9]/tmp2[,7:9]
+    
+    # Result
+    bootresults[[rep]] <- tmpratio
+    
+  }
+  
+  # Results
+  rows <- nrow(riskratio)
+  cols <- ncol(riskratio)
+  lowratio <- matrix(data=NA,nrow=rows,ncol=cols)
+  higratio <- matrix(data=NA,nrow=rows,ncol=cols)
+  sigratio <- matrix(data=NA,nrow=rows,ncol=cols)
+  
+  for(i in 1:rows) {
+    for(j in 1:cols) {
+      tmp <- unlist(lapply(bootresults,function(x) x[i,j]))
+      qtmp <- quantile(tmp,probs=c(0.025,0.975))
+      lowratio[i,j] <- qtmp[1]
+      higratio[i,j] <- qtmp[2]
+      sigratio[i,j] <- as.numeric(!any(qtmp<0))
+    }
+  }
+  
+  
 ### Save results ###############################################################
   
+  # Combine with bootstrap
+  colnames(lowratio) <- c("rr_physical_95low","rr_stress_95low","rr_poverty_95low")
+  colnames(higratio) <- c("rr_physical_95up","rr_stress_95up","rr_poverty_95up")
+  quality <- cbind(quality,lowratio,higratio)
+
+  # Save  
   write_xlsx(quality,"Results/quality.xlsx")
